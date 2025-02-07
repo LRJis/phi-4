36d4f1ec31832f2a711b8dc6fc56ddd2786ff189
LRJis <47349007+LRJis@users.noreply.github.com>
2025-02-07 10:04:03 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 chmod -R 744 /kaggle/working
 sudo chown root:root /
-chmod 600 /kaggle/working/.ssh/id_rsa/authorized_keys
+chmod 600 /kaggle/working/.ssh/authorized_keys
 sudo apt install -y nano
 sudo apt install -y openssh-server
